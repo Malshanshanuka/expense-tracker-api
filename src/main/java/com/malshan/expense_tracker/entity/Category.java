5dbe3f57e2474396347dfe281b16a765f653c091
Malshanshanuka <malshanshanuka1216@gmail.com>
2026-08-14 21:37:24 +0530

    user entity and category entity

---
+++ src/main/java/com/malshan/expense_tracker/entity/Category.java
@@ -0,0 +1,25 @@
+package com.malshan.expense_tracker.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
